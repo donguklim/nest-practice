87895b644b70fedaac380074880558815261fc2e
KUDANSHI\dlim7 <dlim7272@gmail.com>
2025-05-19 15:19:26 +0900

    add user model

---
+++ auth-service/src/auth/constants.ts
@@ -0,0 +1,6 @@
+export enum UserRole {
+  USER = 4,
+  OPERATOR = 3,
+  AUDITOR = 2,
+  ADMIN = 1,
+}
---
+++ auth-service/src/auth/db/user.schema.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+import { UserRole } from 'auth/constants';
+
+export const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: Number, enum: Object.values(UserRole) },
+});
+
+UserSchema.index({ username: 'hashed' });

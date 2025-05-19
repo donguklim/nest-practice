347b75d94cfd86ea5995da2bec630a6178e95b26
KUDANSHI\dlim7 <dlim7272@gmail.com>
2025-05-19 18:25:11 +0900

    forget sharding

--- init-shard.js
+++
@@ -1,4 +0,0 @@
-sh.addShard("shardReplSet1/account-shard1:27018");
-sh.addShard("shardReplSet2/account-shard2:27019");
-sh.enableSharding("auth");
-sh.shardCollection("auth.users", { username: "hashed" });

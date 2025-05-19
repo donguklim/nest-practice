32f485897db3f0577b3438f69da9131fdace299f
KUDANSHI\dlim7 <dlim7272@gmail.com>
2025-05-19 15:20:31 +0900

    add auth service

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserSchema } from './auth/db/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'jwt_secret_key',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ auth-service/src/auth/service.ts
@@ -0,0 +1,43 @@
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserRole } from 'auth/constants';
+import { LoginDto } from 'auth/dto/login.dto';
+import { User } from 'auth/user.interface';
+
+@Injectable()
+export class Service {
+  constructor(
+    @Inject('USER_MODEL')
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userModel.findOne({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user.toObject();
+      return result;
+    }
+    return null;
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.validateUser(loginDto.username, loginDto.password);
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+    return {
+      access_token: this.jwtService.sign({
+        sub: user._id,
+        username: user.username,
+        role: user.role,
+      }),
+    };
+  }
+
+  async registerUser(username: string, password: string, role: UserRole) {
+    const hash = await bcrypt.hash(password, 10);
+    const user = new this.userModel({ username, password: hash, role: role });
+    return user.save();
+  }
+}

ef5f04bb2cf212c49be1d895c87f5b4f93ecf55f
KUDANSHI\dlim7 <dlim7272@gmail.com>
2025-05-20 12:03:48 +0900

    setup project

---
+++ event-service/src/swagger.ts
@@ -0,0 +1,19 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+// import * as YAML from 'yaml';
+// import { writeFileSync } from 'fs';
+
+export function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('NestJS Study API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  // ✅ Write YAML file
+  // const yamlString = YAML.stringify(document);
+  // writeFileSync('./swagger.yaml', yamlString);
+}
